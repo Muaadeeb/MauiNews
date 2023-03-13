80f7bc416ef8fc44a80dff42c48aedac9d0552c2
Timothy Ingledue <timothy.ingledue@orion.com>
2023-03-13 10:39:07 -0500

    Using Microsoft Xamarian Essentials - Share -Adding  I/OS  permissions with Info.plist (using generic PList editor).

--- News/News/Pages/NewsDetail.xaml.cs
+++ News/News/Pages/NewsDetail.xaml.cs
@@ -9,4 +9,10 @@ public partial class NewsDetail : ContentPage
         Title.Text = article.Title;
         Content.Text = article.Content;
     }
+
+    private void Share_Clicked(object sender, EventArgs e)
+    {
+        // To share on social media - use the MAUI plugin.   Microsoft.Maui.Essentials.
+
+    }
 }
