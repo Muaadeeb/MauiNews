be7c124d87bd55243810bedf7eb1b615299a609c
Timothy Ingledue <timothy.ingledue@orion.com>
2023-03-13 14:10:49 -0500

    Had to change control from Shared to TbShare due to namespace conflicts.

--- News/News/Pages/NewsDetail.xaml.cs
+++ News/News/Pages/NewsDetail.xaml.cs
@@ -2,17 +2,24 @@ namespace News.Pages;
 
 public partial class NewsDetail : ContentPage
 {
+    private string uri;
+
     public NewsDetail(Article article)
     {
         InitializeComponent();
         ImgNews.Source = article.Image;
         Title.Text = article.Title;
         Content.Text = article.Content;
+        uri = article.Url;
     }
 
-    private void Share_Clicked(object sender, EventArgs e)
+    private async void TbShare_Clicked(object sender, EventArgs e)
     {
         // To share on social media - use the MAUI plugin.   Microsoft.Maui.Essentials.
-
+        await Share.RequestAsync(new ShareTextRequest
+        {
+            Uri = uri,
+            Title = "Share Web Link"
+        });
     }
 }
